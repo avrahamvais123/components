4cc6e45f0d118adf433e504f961f6303cef1b236
avrahamvais123 <avrahamvais123@gmail.com>
2025-09-11 01:29:16 +0300

    implement deep signal store for state management and update page component to use new signals

--- app/page.js
+++ app/page.js
@@ -1,16 +1,20 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
-import { useSignal } from "@preact/signals-react";
+import {  useDeepSignal } from "deepsignal/react";
+
+//const store = deepSignal({ count: 0, test: "hello" });
 
 export default function Home() {
-  const count = useSignal(0, { name: "count" });
+  const store = useDeepSignal({ count: 0, test: "hello" });
+  //useSignals(); // חשוב כדי ש-React "ישמע" לקריאות לסיגנלים בזמן רנדר
   return (
     <div className="h-dvh test">
       <div className="flex h-full flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello, Preact Signal!</h1>
-        <p className="text-lg">Count: {count}</p>
-        <Button onClick={() => count.value++}>Increment</Button>
+        <h1 className="text-2xl font-bold">Hello, DeepSignal!</h1>
+        <p className="text-lg">Count: {store.count}</p>
+        <p className="text-lg">test: {store.test}</p>
+        <Button onClick={() => (store.count += 1)}>Increment</Button>
+        <Button onClick={() => (store.test = "world")}>change test</Button>
       </div>
     </div>
   );
