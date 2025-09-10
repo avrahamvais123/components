2dd929bdb3fa14e9682076c33f826d44e8c5242c
avrahamvais123 <avrahamvais123@gmail.com>
2025-09-10 23:18:41 +0300

    add layout utility classes, implement Preact signals for counter functionality, and update dependencies

--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSignal } from "@preact/signals-react";
 
 export default function Home() {
+  const count = useSignal(0, { name: "count" });
   return (
     <div className="h-dvh test">
+      <div className="flex h-full flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello, Preact Signal!</h1>
+        <p className="text-lg">Count: {count}</p>
+        <Button onClick={() => count.value++}>Increment</Button>
+      </div>
     </div>
   );
 }
