8805fc0b5766b6b884ee6519bf1c97389ce74528
avrahamvais123 <avrahamvais123@gmail.com>
2025-09-17 18:03:41 +0300

    fix: correct destructuring of searchParams in Page component for improved clarity

--- app/(pages)/categories/page.js
+++ app/(pages)/categories/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = async (porps) => {
-  const { searchParams } = await porps;
+  const searchParams = await porps.searchParams;
   const name = searchParams?.name ?? "";
   return <div className="min-h-[calc(100dvh-5rem)] center">{name}</div>;
 };
