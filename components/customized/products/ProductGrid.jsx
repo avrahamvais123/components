91d2d687daad90644b6659e5a35f8c2bfa70355c
avrahamvais123 <avrahamvais123@gmail.com>
2025-09-21 07:18:12 +0300

    feat: update ProductGrid background for dark mode support

--- components/customized/products/ProductGrid.jsx
+++ components/customized/products/ProductGrid.jsx
@@ -33,7 +33,7 @@ export default function ProductGrid({
   const safeProducts = useMemo(() => sanitizeProducts(products), [products]);
 
   return (
-    <div className="full p-8 min-h-[calc(100dvh-4rem)] bg-white">
+    <div className="full p-8 min-h-[calc(100dvh-4rem)] bg-white dark:bg-neutral-950">
       <div className="max-w-9xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
         {safeProducts.map((p, idx) => (
           <ProductCard
